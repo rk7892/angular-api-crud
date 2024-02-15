18a954c08b1f2746f59befe64d6bc5a0b4bf5898
rk7892 <ranjithraina7952@gmail.com>
2024-02-15 14:49:32 +0530

    search

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CustomerService } from '../service/customer.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet, FormsModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -14,6 +15,8 @@ export class ListComponent {
 
   service = inject(CustomerService);
   getUserArray: any[] = [];
+  filteredUsers: any[] = [];
+  filterBy: any;
 
   constructor(){
     this.getUserList();
@@ -29,5 +32,31 @@ export class ListComponent {
     })
   }
 
+  // filterList() {
+  //   this.filteredUsers = [...this.getUserArray.filter(user => this.getUserArray.name.includes(this.filterBy))];
+  // }
  
+  searchByProperty(event: Event, property: string) {
+    const searchText = (event.target as HTMLInputElement).value;
+    if (!searchText) {
+      this.getUserList();
+    } else {
+      this.getUserArray = this.getUserArray.filter(user => {
+        return user[property].toLowerCase().includes(searchText.toLowerCase());
+      });
+    }
+  }
+  
+  
+  searchName(event: Event) {
+    this.searchByProperty(event, 'name');
+  }
+
+  searchPhone(event: Event) {
+    this.searchByProperty(event, 'phone');
+  }
+  
+  searchDesignation(event: Event) {
+    this.searchByProperty(event, 'website');
+  }
 }
